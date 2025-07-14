b5cbc7724a3404e191469681f7c1e7ef2fdbe213
bigun-hettiarachchi <IT21144844@my.sliit.lk>
2025-07-14 14:43:24 +0530

    update with path

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -14,6 +16,7 @@ dotenv.config();
 
 // const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "50mb" })); // increase limit to 50mb or as needed
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -29,6 +32,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
   connectDB();
